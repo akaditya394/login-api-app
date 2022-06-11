2df5a48f27268fbf3e66ec3604d7eb473291d7e1
Aditya <akaditya394@gmail.com>
2022-06-11 19:51:31 +0530

    font added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,7 +7,9 @@ function Header(props) {
   return (
     <header className={`${classes.header} ${props.className}`}>
       <div className={classes.content}>
-        <h1 className={classes.brandName}>ATools</h1>
+        <h1 className={classes.brandName}>
+          ATools<span style={{ color: "red" }}>.</span>
+        </h1>
         <Button className={classes.trialBtn}>Start Free Trial</Button>
         <Button className={classes.headerLoginBtn}>Login</Button>
       </div>
